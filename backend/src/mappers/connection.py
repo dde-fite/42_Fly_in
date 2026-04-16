33ca26f439c0699844ea10a20b324ece6ccaa751
dde-fite <nora@defitero.com>
2026-04-17 01:01:01 +0200

    backend: minor changes in mappers

--- backend/src/mappers/connection.py
+++ backend/src/mappers/connection.py
@@ -1,9 +1,9 @@
-from src.schema import ResponseConnection, HubRef
+from src.schema import ResponseConnection
 from src.models import Connection
 
 
 def connection_to_schema(c: Connection) -> ResponseConnection:
     return ResponseConnection(
-        hubs=[HubRef(hub.id) for hub in c.hubs],
+        hubs=[hub.id for hub in c.hubs],
         capacity=c.capacity
     )
--- backend/src/mappers/hub.py
+++ backend/src/mappers/hub.py
@@ -5,9 +5,9 @@ from src.schema import ResponseHub
 def hub_to_schema(h: Hub) -> ResponseHub:
     return ResponseHub(
         name=h.name,
-        position=h.position,
-        access=h.access,
-        color=h.color.as_named(),
+        position=(h.position.x, h.position.y),
+        access=h.access.value,
+        color=h.color.as_named() if h.color else None,
         drones=[drone.id for drone in h.drones],
         capacity=h.capacity,
         connections=[con.id for con in h.connections]
