9aee9fb30f5fc5e55e1dd484fc110b4578c192bf
dde-fite <nora@defitero.com>
2026-04-17 00:53:15 +0200

    backend: finished connection mapper

--- backend/src/mappers/connection.py
+++ backend/src/mappers/connection.py
@@ -1,8 +1,9 @@
-from src.schema import ResponseConnection
+from src.schema import ResponseConnection, HubRef
 from src.models import Connection
 
 
 def connection_to_schema(c: Connection) -> ResponseConnection:
     return ResponseConnection(
-
+        hubs=[HubRef(hub.id) for hub in c.hubs],
+        capacity=c.capacity
     )
