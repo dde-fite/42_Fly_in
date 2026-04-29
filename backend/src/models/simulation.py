0c44ae6c91caedcba7af96780539c9376ea79942
dde-fite <nora@defitero.com>
2026-04-29 22:45:28 +0200

    backend: simulation does not initializes Turn

--- backend/src/models/simulation.py
+++ backend/src/models/simulation.py
@@ -1,16 +1,12 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
 from pydantic import BaseModel
 from .turn import Turn
-
-if TYPE_CHECKING:
-    from .hub import Hub
-    from .drone import Drone
-    from .connection import Connection
+from .hub import Hub
+from .drone import Drone
+from .connection import Connection
 
 
 class Simulation(BaseModel):
-    turns: Turn = Turn(0)
+    turn: Turn
     hubs: set[Hub]
     origin: Hub
     destination: Hub
