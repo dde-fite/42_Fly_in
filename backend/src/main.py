ffe954c1f87227c8d04e788291e068689cd2f788
dde-fite <nora@defitero.com>
2026-06-04 21:46:05 +0200

    backend: [feat] cors middleware for connecting with frontend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,23 @@
 import src.models._rebuild
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.core.logging import setup_logging
 from src.api import routes
 
 setup_logging()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(routes.router, prefix="/api")
