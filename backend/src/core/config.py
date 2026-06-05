cf0ebc7bf355d6cb9126b71a956f64fd16b09ae1
dde-fite <nora@defitero.com>
2026-06-05 06:03:47 +0200

    backend: [feat] STRICT_PARSER env var defaults to True

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -5,7 +5,7 @@ class Config(BaseSettings):
     FRONTEND_URL: str = "http://localhost:3000"
     LOG_LEVEL: str = "DEBUG"
     EXTENDED_LOGGING: bool = False
-    STRICT_PARSER: bool = False
+    STRICT_PARSER: bool = True
 
 
 config = Config()
