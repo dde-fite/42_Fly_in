61937a041844b8e8b715f1c8a02ee06f1d6c6c61
dde-fite <nora@defitero.com>
2026-06-06 06:43:45 +0200

    frontend: [feat] setup tailwind for styling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -7,7 +8,7 @@ export default defineConfig(({ mode }) => {
   // `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       // Provide an explicit app-level constant derived from an env var.
       __FRONTEND_URL__: JSON.stringify(env.FRONTEND_URL),
